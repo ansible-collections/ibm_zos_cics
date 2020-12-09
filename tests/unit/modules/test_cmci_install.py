d30a4b56dac1db4935755871860f6ec5e1a3aa36
Stew Francis <stewartfrancis@uk.ibm.com>
2020-12-09 18:45:32 +0000

    Add Tom's test

--- tests/unit/modules/test_cmci_install.py
+++ tests/unit/modules/test_cmci_install.py
@@ -8,7 +8,7 @@ __metaclass__ = type
 
 from ansible_collections.ibm.ibm_zos_cics.plugins.modules import cmci_install
 from ansible_collections.ibm.ibm_zos_cics.tests.unit.helpers.cmci_helper import (
-    HOST, PORT, CONTEXT, od, create_records_response, body_matcher, cmci_module, CMCITestHelper
+    HOST, PORT, CONTEXT, SCOPE, od, create_records_response, body_matcher, cmci_module, CMCITestHelper
 )
 
 
@@ -102,4 +102,64 @@ def test_bas_install(cmci_module):  # type: (CMCITestHelper) -> None
             type='cicsdefinitionbundle',
             location='BAS'
         )
+    ))
+
+
+def test_install_csd_criteria_parameter(cmci_module):  # type: (CMCITestHelper) -> None
+    # TODO: The parameters field for criteria is intentionally stubbing a bad request here. The "+'s" will be
+    # removed in a future fix
+    record = dict(
+        changeagent='CSDAPI',
+        changeagrel='0730',
+        csdgroup='DUMMY',
+        name='DUMMY'
+    )
+    cmci_module.stub_records(
+        'PUT',
+        'cicsdefinitionprogram',
+        [record],
+        scope=SCOPE,
+        parameters='?CRITERIA=%28%28NAME%3DDUMMY%29+AND+%28DEFVER%3D0%29+AND+'
+                   '%28CSDGROUP%3DDUMMY%29%29&PARAMETER=CSDGROUP%28DUMMY%29',
+        additional_matcher=body_matcher(od(
+            ('request', od(
+                ('action', od(
+                    ('@name', 'CSDINSTALL')
+                ))
+            ))
+        ))
+    )
+
+    cmci_module.expect({
+        'changed': True,
+        'request': {
+            'body':
+                '<request>'
+                '<action name="CSDINSTALL"></action>'
+                '</request>',
+            'method': 'PUT',
+            'url': 'http://winmvs2c.hursley.ibm.com:26040/CICSSystemManagement/cicsdefinitionprogram/CICSEX56/IYCWEMW2',
+            'params': {
+                'PARAMETER': 'CSDGROUP(DUMMY)',
+                'CRITERIA': '((NAME=DUMMY) AND (DEFVER=0) AND (CSDGROUP=DUMMY))'
+            }
+        },
+        'response': {
+            'body': create_records_response('cicsdefinitionprogram', [record]),
+            'reason': 'OK',
+            'status_code': 200}
+    })
+
+    cmci_module.run(cmci_install, dict(
+        cmci_host=HOST,
+        cmci_port=PORT,
+        context=CONTEXT,
+        scope=SCOPE,
+        security_type='none',
+        resource=dict(
+            type='cicsdefinitionprogram',
+            location='CSD',
+        ),
+        criteria="((NAME=DUMMY) AND (DEFVER=0) AND (CSDGROUP=DUMMY))",
+        parameter='CSDGROUP(DUMMY)'
     ))
