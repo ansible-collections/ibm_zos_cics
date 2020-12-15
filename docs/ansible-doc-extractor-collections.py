c642dae08bd7db006c1d56bfa1d564676f539345
Stew Francis <stewartfrancis@uk.ibm.com>
2020-12-15 01:11:04 +0000

    Util to make ansible-doc-generator work for ansible_collections

---
+++ docs/ansible-doc-extractor-collections.py
@@ -0,0 +1,10 @@
+import re
+import sys
+from ansible_doc_extractor.cli import main
+from ansible.utils.collection_loader import AnsibleCollectionLoader
+
+if __name__ == '__main__':
+    # allow doc-extractor to import code from collections so doc_fragment plugins work
+    sys.meta_path.insert(0, AnsibleCollectionLoader())
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.exit(main())
