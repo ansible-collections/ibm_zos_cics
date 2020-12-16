e18b4d0d682e2e48661c08273093eed63d466c8b
Sophie Green <SOPHIEGR@uk.ibm.com>
2020-12-16 16:19:56 +0000

    Move from imports above documentation

--- plugins/modules/cmci_install.py
+++ plugins/modules/cmci_install.py
@@ -4,6 +4,9 @@
 # Copyright (c) IBM Corporation 2020
 # Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
 
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 DOCUMENTATION = r'''
 ---
 module: cmci_install
@@ -209,9 +212,6 @@ request:
       type: str
 """
 
-
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
 
 from ansible_collections.ibm.ibm_zos_cics.plugins.module_utils.cmci import (
     AnsibleCMCIModule, append_resources_argument
