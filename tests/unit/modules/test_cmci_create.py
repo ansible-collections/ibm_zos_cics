fd45603939e6268bc0834a40c653e255c494b4c2
Thomas Latham <52537288+Tom-Latham@users.noreply.github.com>
2020-12-18 20:01:18 +0000

    BAS query/ delete/ create tests

--- tests/unit/modules/test_cmci_create.py
+++ tests/unit/modules/test_cmci_create.py
@@ -39,27 +39,17 @@ def test_csd_create(cmci_module):  # type: (CMCITestHelper) -> None
         ))
     )
 
-    cmci_module.expect({
-        'changed': True,
-        'request': {
-            'url': 'https://winmvs2c.hursley.ibm.com:26040/CICSSystemManagement/'
-                   'cicsdefinitionbundle/CICSEX56/IYCWEMW2',
-            'method': 'POST',
-            'body': '<request><create>'
-                    '<parameter name="CSD"></parameter>'
-                    '<attributes name="bar" bundledir="/u/bundles/bloop" csdgroup="bat"></attributes>'
-                    '</create></request>'
-        },
-        'connect_version': '0560',
-        'cpsm_reason': '',
-        'cpsm_reason_code': 0,
-        'cpsm_response': 'OK',
-        'cpsm_response_code': 1024,
-        'http_status': 'OK',
-        'http_status_code': 200,
-        'record_count': 1,
-        'records': [record]
-    })
+    cmci_module.expect(
+        result(
+            'https://winmvs2c.hursley.ibm.com:26040/CICSSystemManagement/'
+            'cicsdefinitionbundle/CICSEX56/IYCWEMW2',
+            record,
+            '<request><create>'
+            '<parameter name="CSD"></parameter>'
+            '<attributes name="bar" bundledir="/u/bundles/bloop" csdgroup="bat"></attributes>'
+            '</create></request>'
+        )
+    )
 
     cmci_module.run(cmci_create, dict(
         cmci_host=HOST,
@@ -72,3 +62,75 @@ def test_csd_create(cmci_module):  # type: (CMCITestHelper) -> None
         )],
         attributes=record
     ))
+
+
+def test_bas_create(cmci_module):  # type: (CMCITestHelper) -> None
+    record = dict(
+        RGSCOPE="BAS1",
+        RESDESC = "BASICB11",
+        AUTOINST = "NO"
+    )
+    cmci_module.stub_records(
+        'POST',
+        'cicsdefinitionbundle',
+        [record],
+        scope='IYCWEMW2',
+        additional_matcher=body_matcher(od(
+            ('request', od(
+                ('create', od(
+                    ('parameter', od(
+                        ('@name', 'BAS')
+                    )),
+                    ('attributes', od(
+                        ('@RGSCOPE', 'BAS1'),
+                        ('@RESDESC', 'BASICB11'),
+                        ('@AUTOINST', 'NO')
+                    ))
+                ))
+            ))
+        ))
+    )
+
+    cmci_module.expect(
+        result(
+            'https://winmvs2c.hursley.ibm.com:26040/CICSSystemManagement/'
+            'cicsdefinitionbundle/CICSEX56/IYCWEMW2',
+            record,
+            '<request><create>'
+            '<parameter name="BAS"></parameter>'
+            '<attributes RGSCOPE="BAS1" RESDESC="BASICB11" AUTOINST="NO"></attributes>'
+            '</create></request>'
+        )
+    )
+
+    cmci_module.run(cmci_create, dict(
+        cmci_host=HOST,
+        cmci_port=PORT,
+        context=CONTEXT,
+        scope='IYCWEMW2',
+        type='cicsdefinitionbundle',
+        parameters=[dict(
+            name='BAS'
+        )],
+        attributes=record
+    ))
+
+
+def result(url, record, body):
+    return {
+        'changed': True,
+        'connect_version': '0560',
+        'cpsm_reason': '',
+        'cpsm_reason_code': 0,
+        'cpsm_response': 'OK',
+        'cpsm_response_code': 1024,
+        'http_status': 'OK',
+        'http_status_code': 200,
+        'record_count': 1,
+        'records': [record],
+        'request': {
+            'url': url,
+            'method': 'POST',
+            'body': body
+        },
+    }
