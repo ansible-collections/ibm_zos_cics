ddcf104d0b11ad5fefde81fc89d03be5f0120f27
Sophie Green <SOPHIEGR@uk.ibm.com>
2021-02-03 10:31:58 +0000

    Review updates for bundle install IT

--- tests/integration/targets/cics_cmci/runme.sh
+++ tests/integration/targets/cics_cmci/runme.sh
@@ -10,5 +10,5 @@ ansible-playbook -e "@cmci-variables.yml" playbooks/cmci_invalid_credentials.yml
 ansible-playbook -e "@cmci-variables.yml" playbooks/cmci_incorrect_scope.yml
 ansible-playbook -e "@cmci-variables.yml" playbooks/cmci_incorrect_context.yml
 ansible-playbook -e "@cmci-variables.yml" playbooks/cics_cmci_http.yml
-ansible-playbook -e "@cmci-variables.yml" playbooks/cmci_create_bundle_failure.yml
+ansible-playbook -e "@cmci-variables.yml" playbooks/cmci_install_bundle_failure.yml
 ansible-playbook -e "@cmci-variables.yml" playbooks/cmci_create_pipeline_failure.yml
