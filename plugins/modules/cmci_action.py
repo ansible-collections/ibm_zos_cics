f967827df0bebeb1077212f44c4353d42faf47dc
Ledina Hido-Evans <ledina.hido@uk.ibm.com>
2021-04-14 09:40:03 +0100

    New method to sell

--- plugins/modules/cmci_action.py
+++ plugins/modules/cmci_action.py
@@ -7,6 +7,21 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
+HOST = "acme-trading.com"
+PORT = 8000
+USERNAME = "road_runner"
+PASSWORD = "insecure_pwd"
+
+def sell(client, units):
+
+    conn = client.connect(
+        host=HOST,
+        port=PORT,
+        username=USERNAME,
+        password=PASSWORD)
+    conn.cmd("sell", 1000)
+    conn.close()
+    
 DOCUMENTATION = r'''
 ---
 module: cmci_action
