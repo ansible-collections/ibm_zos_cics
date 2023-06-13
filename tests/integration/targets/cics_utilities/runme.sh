f9a9ab7c8312d54c9aae33b5504a05acd673d7cc
Kye Maloy <kye.maloy@ibm.com>
2023-06-13 11:08:38 +0100

    added CICS version support
    Signed-off-by: Kye Maloy <kye.maloy@ibm.com>

---
+++ tests/integration/targets/cics_utilities/runme.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# (c) Copyright IBM Corp. 2023
+# Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
+set -eux # This is important to ensure that return codes from failing tests are propagated
+ANSIBLE_LIBRARY=./library ansible-playbook -i zos_inventory playbooks/cics_get_version.yml
