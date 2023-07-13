0a7c18cf2f04c43c1af25b46a73d8fb92c997d5b
enam-khan <enam.khan@uk.ibm.com>
2023-07-13 15:56:51 +0100

    Inital integration tests for local catalog

---
+++ tests/integration/targets/cics_local_catalog/runme.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# (c) Copyright IBM Corp. 2023
+# Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
+
+set -eux
+export ANSIBLE_COLLECTIONS_PATH=/root/.ansible/collections:$ANSIBLE_COLLECTIONS_PATH
+ansible-playbook -i zos_inventory -e "@provisioning-variables.yml" playbooks/initial_catalog.yml
+ansible-playbook -i zos_inventory -e "@provisioning-variables.yml" playbooks/absent_catalog.yml
+ansible-playbook -i zos_inventory -e "@provisioning-variables.yml" playbooks/check_bad_lcd_location.yml
+ansible-playbook -i zos_inventory -e "@provisioning-variables.yml" playbooks/check_output.yml
