2c5c2338f03a5315f02d0f146e7dc7f5c1e69778
enam-khan <enam.khan@uk.ibm.com>
2023-07-14 15:54:49 +0100

    Update

--- tests/integration/targets/cics_local_catalog/runme.sh
+++ tests/integration/targets/cics_local_catalog/runme.sh
@@ -12,5 +12,5 @@ ZOAU_ENV="/root/ansible_collections/ibm/ibm_zos_cics/tests/integration/variables
 ansible-playbook -i $INV_PATH -e "@$VAR_PATH" -e "@$ZOAU_ENV" playbooks/initial_catalog.yml
 ansible-playbook -i $INV_PATH -e "@$VAR_PATH" -e "@$ZOAU_ENV" playbooks/absent_catalog.yml
 ansible-playbook -i $INV_PATH -e "@$VAR_PATH" -e "@$ZOAU_ENV" playbooks/check_output.yml
-ansible-playbook -i $INV_PATH -e "@$VAR_PATH" -e "@$ZOAU_ENV" playbooks/check_bad_gcd_location.yml
+ansible-playbook -i $INV_PATH -e "@$VAR_PATH" -e "@$ZOAU_ENV" playbooks/check_bad_lcd_location.yml
 ansible-playbook -i $INV_PATH -e "@$VAR_PATH" -e "@$ZOAU_ENV" playbooks/check_uppercase_location.yml
