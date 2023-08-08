dbf22a9d7b3ee51785160f7817ef849216bdf16f
Andrew Twydell <andrew.twydell@ibm.com>
2023-08-08 12:06:45 +0100

    local catalog input parameter templating
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- plugins/module_utils/parameter_templating.py
+++ plugins/module_utils/parameter_templating.py
@@ -7,10 +7,10 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 
-def template_dsn(self, task_vars, var_name, replace_val, template):
+def template_dsn(_templar, task_vars, var_name, replace_val, template):
     cpy = task_vars.copy()
     cpy.update({var_name: replace_val})
-    return self._templar.copy_with_new_env(
+    return _templar.copy_with_new_env(
         variable_start_string="<<",
         variable_end_string=">>",
         available_variables=cpy
