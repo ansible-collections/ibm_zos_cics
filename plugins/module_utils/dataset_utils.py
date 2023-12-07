e2933291be354715292c32b2c6c55f2dfd01cd27
Andrew Twydell <andrew.twydell@ibm.com>
2023-12-07 23:28:18 +0000

    allow idcams to not have INDEX
    Signed-off-by: Andrew Twydell <andrew.twydell@ibm.com>

--- plugins/module_utils/dataset_utils.py
+++ plugins/module_utils/dataset_utils.py
@@ -86,13 +86,17 @@ def _get_dataset_size_unit(unit_symbol):  # type: (str) -> str
 
 
 def _build_idcams_define_cmd(dataset):  # type: (Dict) -> str
-    return '''
-    DEFINE CLUSTER ({0}) -
-    DATA ({1}) -
-    INDEX({2})
-    '''.format(_build_idcams_define_cluster_parms(dataset),
-               _build_idcams_define_data_parms(dataset),
-               _build_idcams_define_index_parms(dataset))
+    index_statement = (
+        f""" -
+    INDEX({_build_idcams_define_index_parms(dataset)})"""
+        if dataset.get("INDEX", None)
+        else ""
+    )
+
+    return f"""
+    DEFINE CLUSTER ({_build_idcams_define_cluster_parms(dataset)}) -
+    DATA ({_build_idcams_define_data_parms(dataset)}){index_statement}
+    """
 
 
 def _build_idcams_define_cluster_parms(dataset):  # type: (Dict) -> str
