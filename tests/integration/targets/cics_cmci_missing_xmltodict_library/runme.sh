86dac58f2789dcc6fa2d15a0ad7869fed9e1afa5
Andrew Hughes <Andrew.Hughes1@ibm.com>
2024-03-27 14:02:08 +0000

    Copy build changes from feature/catalog

--- tests/integration/targets/cics_cmci_missing_xmltodict_library/runme.sh
+++ tests/integration/targets/cics_cmci_missing_xmltodict_library/runme.sh
@@ -2,4 +2,6 @@
 # (c) Copyright IBM Corp. 2021
 # Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
 set -eux # This is important to ensure that return codes from failing tests are propagated
+if pip show xmltodict 2>&1 | grep -q 'Package(s) not found'; then 
 ansible-playbook playbooks/cmci_missing_xmltodict.yml
+fi
