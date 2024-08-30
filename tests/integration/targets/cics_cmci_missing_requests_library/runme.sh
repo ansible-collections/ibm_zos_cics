6dfb958029bc5a4164f14805d83fa6ddef92c5a0
Andrew Hughes <Andrew.Hughes1@ibm.com>
2024-08-30 14:08:32 +0100

    Make integration tests work if environment is correct

--- tests/integration/targets/cics_cmci_missing_requests_library/runme.sh
+++ tests/integration/targets/cics_cmci_missing_requests_library/runme.sh
@@ -2,6 +2,12 @@
 # (c) Copyright IBM Corp. 2021,2024
 # Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
 set -eux # This is important to ensure that return codes from failing tests are propagated
+
 if pip show requests 2>&1 | grep -q 'Package(s) not found'; then 
-ansible-playbook playbooks/cmci_missing_requests.yml
+    ansible-playbook playbooks/cmci_missing_requests.yml
+else
+    CURRENT_PKG=$(pip freeze | grep requests)
+    pip uninstall requests -y
+    ansible-playbook playbooks/cmci_missing_requests.yml
+    pip install "$CURRENT_PKG"
 fi
