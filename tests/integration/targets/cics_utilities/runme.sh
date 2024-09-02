6fe9d33b703668be6dfa6f6f4627158096e16d0f
Andrew Hughes <Andrew.Hughes1@ibm.com>
2024-09-02 21:56:36 +0100

    Remove redundant vars for utility tests

--- tests/integration/targets/cics_utilities/runme.sh
+++ tests/integration/targets/cics_utilities/runme.sh
@@ -5,9 +5,8 @@ set -eux # This is important to ensure that return codes from failing tests are 
 
 export ANSIBLE_LIBRARY=./library
 
-VAR_PATH="$ANSIBLE_COLLECTIONS_PATH/ansible_collections/ibm/ibm_zos_cics/tests/integration/variables/utilities.yml"
 INV_PATH="$ANSIBLE_COLLECTIONS_PATH/ansible_collections/ibm/ibm_zos_cics/tests/integration/inventory_zos.yml"
 ZOS_ENV="$ANSIBLE_COLLECTIONS_PATH/ansible_collections/ibm/ibm_zos_cics/tests/integration/variables/zos.yml"
 
 ansible-playbook -i "$INV_PATH" -e "@$ZOS_ENV" playbooks/success.yml
-ansible-playbook -i "$INV_PATH" -e "@$VAR_PATH" -e "@$ZOS_ENV" playbooks/failure.yml
+ansible-playbook -i "$INV_PATH" -e "@$ZOS_ENV" playbooks/failure.yml
